71708f7bde908f136bb78e263e1f32b4aa01472f
Liisagrant <lisagrant1990@gmail.com>
2023-04-26 20:34:17 +0200

    created the ispo card and added linking to accommodations

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Herobanner from './Herobanner';
 import ImageGrid from './ImageGrid';
+import CardInspo from './CardInspo';
 
 function Home() {
   return (
     <div>
       <Herobanner />
+      <CardInspo />
       <ImageGrid />
     </div>
   );
