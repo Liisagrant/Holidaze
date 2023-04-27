eecb19b87b5d1d30c0adeaad0c47cd361558328a
Liisagrant <lisagrant1990@gmail.com>
2023-04-27 12:11:28 +0200

    created the sellingpoints and the heading over also some font fix for all components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Herobanner from './Herobanner';
 import ImageGrid from './ImageGrid';
 import CardInspo from './CardInspo';
+import SellPoint from './SellPoint';
 
 function Home() {
   return (
     <div>
       <Herobanner />
       <CardInspo />
+      <SellPoint />
       <ImageGrid />
     </div>
   );
