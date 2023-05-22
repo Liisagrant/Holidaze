7d4d3dca92126f3f74cbd83dd8fbd89c96dab243
Liisagrant <lisagrant1990@gmail.com>
2023-05-22 11:49:47 +0200

    started on working on an error component

--- src/Pages/Accommodation/Accommodations.jsx
+++ src/Pages/Accommodation/Accommodations.jsx
@@ -1,5 +1,6 @@
 import AllAccommodations from './AllAccommodations';
 import BreadCrumbs from '../../Global/BreadCrumbs';
+import Error from '../../Layout/Error';
 
 function Accommodations() {
   const breadcrumb = [
@@ -9,6 +10,7 @@ function Accommodations() {
   return (
     <div>
       <div className="mt-40 max-w-7xl mx-auto">
+        <Error />
         <BreadCrumbs breadcrumb={breadcrumb} />
         <AllAccommodations />
       </div>
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import VenuesSlice from './modules/VenuesSlice';
 import ProfileSlice from './modules/ProfileSlice';
 import loaderSlice from './modules/loaderSlice';
+import errorSlice from './modules/errorSlice';
 
 const reducer = {
   Venues: VenuesSlice,
   Profile: ProfileSlice,
   loader: loaderSlice,
+  error: errorSlice,
 };
 
 const store = configureStore({
