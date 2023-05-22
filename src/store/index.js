a91c5e1ca75158bf3c5aeafa88453a172238bb80
Liisagrant <lisagrant1990@gmail.com>
2023-05-22 15:00:40 +0200

    removed some files that made issues

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import VenuesSlice from './modules/VenuesSlice';
 import ProfileSlice from './modules/ProfileSlice';
 import loaderSlice from './modules/loaderSlice';
-import ErrorSlice from './modules/ErrorSlice';
 
 const reducer = {
   Venues: VenuesSlice,
   Profile: ProfileSlice,
   loader: loaderSlice,
-  error: ErrorSlice,
 };
 
 const store = configureStore({
