f4aa47d0092a31181af3c5383de55a40e84759c7
Liisagrant <lisagrant1990@gmail.com>
2023-05-22 15:36:41 +0200

    fix for first bug fix

--- src/Global/RatingStar.jsx
+++ src/Global/RatingStar.jsx
@@ -1,16 +1,19 @@
-import { Link } from 'react-router-dom';
-
 const RatingStar = ({ venue }) => {
+  const rating =
+    venue.rating && Number.isInteger(venue.rating) && venue.rating <= 5
+      ? venue.rating
+      : 0;
+
   return (
     <div className="my-2">
-      {venue.rating >= 1 ? (
+      {rating >= 1 ? (
         <p className="font-body text-md font-light">
-          {[...Array(venue.rating)].map((_, i) => (
+          {[...Array(rating)].map((_, i) => (
             <span className="text-second" key={i}>
               ★
             </span>
           ))}
-          {[...Array(5 - Math.round(venue.rating))].map((_, i) => (
+          {[...Array(5 - Math.round(rating))].map((_, i) => (
             <span className="text-blue" key={i}>
               ☆
             </span>
