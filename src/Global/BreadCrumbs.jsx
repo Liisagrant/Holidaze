fd91488b5cd338becd62ecabcf6b9b238bac0c31
Liisagrant <lisagrant1990@gmail.com>
2023-05-24 11:31:13 +0200

    small breadcrumb fix

--- src/Global/BreadCrumbs.jsx
+++ src/Global/BreadCrumbs.jsx
@@ -1,19 +1,21 @@
 import { Link } from 'react-router-dom';
 
 const Breadcrumbs = ({ breadcrumb }) => (
-  <nav aria-label="max-w-7xl">
-    <ol className="flex flex-row mb-6 ml-8">
-      {breadcrumb.map((bc, index) => (
-        <li
-          key={bc.path}
-          className="text-sm text-gray-800 font-paragraph flex items-center underline"
-        >
-          <Link to={bc.path}>{bc.name}</Link>
-          {index < breadcrumb.length - 1 && <span className="mx-2">/</span>}
-        </li>
-      ))}
-    </ol>
-  </nav>
+  <div className="max-w-7xl mx-auto px-12">
+    <nav aria-label="max-w-7xl mx-auto">
+      <ol className="flex flex-row mb-6 ml-8">
+        {breadcrumb.map((bc, index) => (
+          <li
+            key={bc.path}
+            className="text-sm text-gray-800 font-paragraph flex items-center underline"
+          >
+            <Link to={bc.path}>{bc.name}</Link>
+            {index < breadcrumb.length - 1 && <span className="mx-2">/</span>}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  </div>
 );
 
 export default Breadcrumbs;
